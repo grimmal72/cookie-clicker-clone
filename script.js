f2057140e4773caae1c3f95c10b630121b8cd8fb
grimmal72 <jordanmalmgren@gmail.com>
2022-12-14 20:25:32 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const cookieHeading = document.querySelector("#cookie-heading");
 const cookieButton = document.querySelector("#cookie-button");
 
-//These are also for accessing DOM elements, but are specifically for the cookie upgrade system
+//These are also for accessing DOM elements, but are specifically for the CPS compounding purchases system
 const autoBakersHeading = document.querySelector("#auto-bakers-heading");
 const autoBakersButton = document.querySelector(".auto-bakers-button");
 const grandmasHeading = document.querySelector("#grandmas-heading");
@@ -15,6 +15,39 @@ const cookieMinesButton = document.querySelector(".cookie-mines-button");
 const cookieFactoriesHeading = document.querySelector("#cookie-factories-heading");
 const cookieFactoriesButton = document.querySelector(".cookie-factories-button");
 
+const cookieBanksHeading = document.querySelector("#cookie-banks-heading");
+const cookieBanksButton = document.querySelector(".cookie-banks-button");
+const cookieTemplesHeading = document.querySelector("#cookie-temples-heading");
+const cookieTemplesButton = document.querySelector(".cookie-temples-button");
+
+const wizardTowersHeading = document.querySelector("#wizard-towers-heading");
+const wizardTowersButton = document.querySelector(".wizard-towers-button");
+
+// Accessing the DOM elements of the one-time upgrades
+const autoBakersOneTimeUpgradeButton = document.querySelector("#upgrade-square-1");
+const grandmasOneTimeUpgradeButton = document.querySelector("#upgrade-square-2");
+const cookieFarmsOneTimeUpgradeButton = document.querySelector("#upgrade-square-3");
+const grandmasOneTimeUpgradeButton2 = document.querySelector("#upgrade-square-4");
+const cookieMinesOneTimeUpgradeButton = document.querySelector("#upgrade-square-5");
+const cookieFactoriesOneTimeUpgradeButton = document.querySelector("#upgrade-square-6");
+const cookieBanksOneTimeUpgradeButton = document.querySelector("#upgrade-square-7");
+const cookieTemplesOneTimeUpgradeButton = document.querySelector("#upgrade-square-8");
+const cookieWizardTowersOneTimeUpgradeButton = document.querySelector("#upgrade-square-9");
+const justFuckingDoItOneTimeUpgradeButton = document.querySelector("#upgrade-square-10");
+
+// Set the multiplier values of the one-time upgrades to 1, so that they can multiply without destroying things (multiplying by zero wouldn't work), and then the value can be updated by the event listener boolean
+let autoBakersOneTimeUpgradeMultiplier = 1;
+let grandmasOneTimeUpgradeMultiplier = 1;
+let cookieFarmsOneTimeUpgradeMultiplier = 1;
+let grandmasOneTimeUpgrade2Multiplier = 1;
+let cookieMinesOneTimeUpgradeMultiplier = 1;
+let cookieFactoriesOneTimeUpgradeMultiplier = 1;
+let cookieBanksOneTimeUpgradeMultiplier = 1;
+let cookieTemplesOneTimeUpgradeMultiplier = 1;
+let cookieWizardTowersOneTimeUpgradeMultiplier = 1;
+let justFuckingDoItOneTimeUpgradeMultiplier = 1;
+
+
 // Game variables
 let cookies = 0;
 let cost = 5;
@@ -25,13 +58,19 @@ let grandmasAmount = 0;
 let cookieFarmsAmount = 0;
 let cookieMinesAmount = 0;
 let cookieFactoriesAmount = 0;
+let cookieBanksAmount = 0;
+let cookieTemplesAmount = 0;
+let wizardTowersAmount = 0;
 
-// Setting the store upgrades to red font-style and grayed out button initially to imply that we can't buy the upgrades.
+// Setting the store compounders to red font-style and grayed out button initially to imply that we can't buy the compounders.
 document.getElementById("auto-bakers-heading").style.color = "red"; // the red text
 document.getElementById("grandmas-heading").style.color = "red";
 document.getElementById("cookie-farms-heading").style.color = "red";
 document.getElementById("cookie-mines-heading").style.color = "red";
 document.getElementById("cookie-factories-heading").style.color = "red";
+document.getElementById("cookie-banks-heading").style.color = "red";
+document.getElementById("cookie-temples-heading").style.color = "red";
+document.getElementById("wizard-towers-heading").style.color = "red";
 
 // Initializing the upgrades images, such as the pictures of grandmas and factories
 let imgAutoBakersUpgrade = document.createElement("img");
@@ -100,7 +139,21 @@ let grandmasPrice = 200;
 let cookieFarmsPrice = 1000;
 let cookieMinesPrice = 2000;
 let cookieFactoriesPrice = 5000;
+let cookieBanksPrice = 10000;
+let cookieTemplesPrice = 100000;
+let wizardTowersPrice = 1000000;
 
+// Purchase prices for one-time upgrades - these will use template literal placeholder variables
+let autoBakersOneTimeUpgradePrice = 10000;
+let grandmasOneTimeUpgradePrice = 20000;
+let cookieFarmsOneTimeUpgradePrice = 30000;
+let grandmasOneTimeUpgrade2Price = 40000;
+let cookieMinesOneTimeUpgradePrice = 50000;
+let cookieFactoriesOneTimeUpgradePrice = 60000;
+let cookieBanksOneTimeUpgradePrice = 70000;
+let cookieTemplesOneTimeUpgradePrice = 80000;
+let cookieWizardTowersOneTimeUpgradePrice = 90000;
+let justFuckingDoItOneTimeUpgradePrice = 100000;
 
 
 // Event listener for the non-automatic cookie button (the one that the player can click a lot if they want)
@@ -111,19 +164,101 @@ cookieButton.addEventListener("click", function() {
 
 let autoBakersCPS = 1; // come back to this after setting up upgrades divs. i'm going to use a template literal on autoBakerButton right after that. it will look like `cookiesPerSecond += 1 * ${upgradesMultiplier}` or `cookiesPerSecond += 1000 * ${upgradesMultiplier}`, where the CPM of 1 belongs to the autobakers and the CPM of 1000 belongs to the factories
 
+// Event listeners for the one-time upgrades
+autoBakersOneTimeUpgradeButton.addEventListener("click", () => {
+    if(cookies >= `${autoBakersOneTimeUpgradePrice}`) {
+        cookies -= `${autoBakersOneTimeUpgradePrice}`;
+        autoBakersOneTimeUpgradeMultiplier = 2;
+       // and then add an attribute of unselectable to the DOM element of <div id="upgrade-square-1"></div>
+    }
+});
+
+grandmasOneTimeUpgradeButton.addEventListener("click", () => {
+    if(cookies >= `${grandmasOneTimeUpgradePrice}`) {
+        cookies -= `${grandmasOneTimeUpgradePrice}`;
+        grandmasOneTimeUpgradeMultiplier = 2;
+       // and then add an attribute of unselectable to the DOM element of <div id="upgrade-square-1"></div>
+    }
+});
+
+cookieFarmsOneTimeUpgradeButton.addEventListener("click", () => {
+    if(cookies >= `${cookieFarmsOneTimeUpgradePrice}`) {
+        cookies -= `${cookieFarmsOneTimeUpgradePrice}`;
+        cookieFarmsOneTimeUpgradeMultiplier = 2;
+       // and then add an attribute of unselectable to the DOM element of <div id="upgrade-square-1"></div>
+    }
+});
+
+grandmasOneTimeUpgradeButton2.addEventListener("click", () => {
+    if(cookies >= `${grandmasOneTimeUpgrade2Price}`) {
+        cookies -= `${grandmasOneTimeUpgrade2Price}`;
+        grandmasOneTimeUpgrade2Multiplier = 4;
+       // and then add an attribute of unselectable to the DOM element of <div id="upgrade-square-1"></div>
+    }
+});
+
+cookieMinesOneTimeUpgradeButton.addEventListener("click", () => {
+    if(cookies >= `${cookieMinesOneTimeUpgradePrice}`) {
+        cookies -= `${cookieMinesOneTimeUpgradePrice}`;
+        cookieMinesOneTimeUpgradeMultiplier = 2;
+       // and then add an attribute of unselectable to the DOM element of <div id="upgrade-square-1"></div>
+    }
+});
+
+cookieFactoriesOneTimeUpgradeButton.addEventListener("click", () => {
+    if(cookies >= `${cookieFactoriesOneTimeUpgradePrice}`) {
+        cookies -= `${cookieFactoriesOneTimeUpgradePrice}`;
+        cookieFactoriesOneTimeUpgradeMultiplier = 2;
+       // and then add an attribute of unselectable to the DOM element of <div id="upgrade-square-1"></div>
+    }
+});
+
+cookieBanksOneTimeUpgradeButton.addEventListener("click", () => {
+    if(cookies >= `${cookieBanksOneTimeUpgradePrice}`) {
+        cookies -= `${cookieBanksOneTimeUpgradePrice}`;
+        cookieBanksOneTimeUpgradeMultiplier = 2;
+       // and then add an attribute of unselectable to the DOM element of <div id="upgrade-square-1"></div>
+    }
+});
+
+cookieTemplesOneTimeUpgradeButton.addEventListener("click", () => {
+    if(cookies >= `${cookieTemplesOneTimeUpgradePrice}`) {
+        cookies -= `${cookieTemplesOneTimeUpgradePrice}`;
+        cookieTemplesOneTimeUpgradeMultiplier = 2;
+       // and then add an attribute of unselectable to the DOM element of <div id="upgrade-square-1"></div>
+    }
+});
+
+cookieWizardTowersOneTimeUpgradeButton.addEventListener("click", () => {
+    if(cookies >= `${cookieWizardTowersOneTimeUpgradePrice}`) {
+        cookies -= `${cookieWizardTowersOneTimeUpgradePrice}`;
+        cookieWizardTowersOneTimeUpgradeMultiplier = 2;
+       // and then add an attribute of unselectable to the DOM element of <div id="upgrade-square-1"></div>
+    }
+});
+
+justFuckingDoItOneTimeUpgradeButton.addEventListener("click", () => {
+    if(cookies >= `${justFuckingDoItOneTimeUpgradePrice}`) {
+        cookies -= `${justFuckingDoItOneTimeUpgradePrice}`;
+        justFuckingDoItOneTimeUpgradeMultiplier = 2;
+       // and then add an attribute of unselectable to the DOM element of <div id="upgrade-square-1"></div>
+    }
+});
+
 // Event listeners for the compounder purchases
 autoBakersButton.addEventListener("click", () => {
     if(cookies >= `${autoBakersPrice}`) {
         cookies -= `${autoBakersPrice}`;
-        cookiesPerSecond += 1; // this line could change to include a template literal. How it will work is that when you buy upgrade squares they will modify the multiplier for this
+        cookiesPerSecond += (1 * `${autoBakersOneTimeUpgradeMultiplier}`); 
         autoBakersAmount += 1;
+        document.getElementById("auto-bakers-heading").style.color = "green";
     }
 });
 
 grandmasButton.addEventListener("click", () => {
     if(cookies >= `${grandmasPrice}`) {
         cookies -= `${grandmasPrice}`;
-        cookiesPerSecond += 25;
+        cookiesPerSecond += (25 * `${grandmasOneTimeUpgradeMultiplier}`);
         grandmasAmount += 1;
         document.getElementById("grandmas-heading").style.color = "green";
     }
@@ -132,7 +267,7 @@ grandmasButton.addEventListener("click", () => {
 cookieFarmsButton.addEventListener("click", () => {
     if(cookies >= `${cookieFarmsPrice}`) {
         cookies -= `${cookieFarmsPrice}`;
-        cookiesPerSecond += 150;
+        cookiesPerSecond += (150 * `${cookieFarmsOneTimeUpgradeMultiplier}`);
         cookieFarmsAmount += 1;
         document.getElementById("cookie-farms-heading").style.color = "green";
     }
@@ -141,7 +276,7 @@ cookieFarmsButton.addEventListener("click", () => {
 cookieMinesButton.addEventListener("click", () => {
     if(cookies >= `${cookieMinesPrice}`) {
         cookies -= `${cookieMinesPrice}`;
-        cookiesPerSecond += 320;
+        cookiesPerSecond += (320 * `${cookieMinesOneTimeUpgradeMultiplier}`);
         cookieMinesAmount += 1;
         document.getElementById("cookie-mines-heading").style.color = "green";
     }
@@ -150,9 +285,36 @@ cookieMinesButton.addEventListener("click", () => {
 cookieFactoriesButton.addEventListener("click", () => {
     if(cookies >= `${cookieFactoriesPrice}`) {
         cookies -= `${cookieFactoriesPrice}`;
-        cookiesPerSecond += 1000;
+        cookiesPerSecond += (1000 * `${cookieFactoriesOneTimeUpgradeMultiplier}`);
         cookieFactoriesAmount += 1;
         document.getElementById("cookie-factories-heading").style.color = "green";
+    }
+});
+
+cookieBanksButton.addEventListener("click", () => {
+    if(cookies >= `${cookieBanksPrice}`) {
+        cookies -= `${cookieBanksPrice}`;
+        cookiesPerSecond += (1000 * `${cookieBanksOneTimeUpgradeMultiplier}`);
+        cookieBanksAmount += 1;
+        document.getElementById("cookie-banks-heading").style.color = "green";
+    }
+});
+
+cookieTemplesButton.addEventListener("click", () => {
+    if(cookies >= `${cookieTemplesPrice}`) {
+        cookies -= `${cookieTemplesPrice}`;
+        cookiesPerSecond += (10000 * `${cookieTemplesOneTimeUpgradeMultiplier}`);
+        cookieTemplesAmount += 1;
+        document.getElementById("cookie-temples-heading").style.color = "green";
+    }
+});
+
+wizardTowersButton.addEventListener("click", () => {
+    if(cookies >= `${wizardTowersPrice}`) {
+        cookies -= `${wizardTowersPrice}`;
+        cookiesPerSecond += (10000 * `${cookieWizardTowersOneTimeUpgradeMultiplier}`);
+        wizardTowersAmount += 1;
+        document.getElementById("wizard-towers-heading").style.color = "green";
     }
 });
 
@@ -171,6 +333,9 @@ window.setInterval(function() {
     cookieFarmsHeading.innerHTML = `Cookie Farms: ${cookieFarmsAmount}`;
     cookieMinesHeading.innerHTML = `Cookie Mines: ${cookieMinesAmount}`;
     cookieFactoriesHeading.innerHTML = `Cookie Factories: ${cookieFactoriesAmount}`;
+    cookieBanksHeading.innerHTML = `Cookie Banks: ${cookieBanksAmount}`;
+    cookieTemplesHeading.innerHTML = `Cookie Temples: ${cookieTemplesAmount}`;
+    wizardTowersHeading.innerHTML = `Wizard Towers: ${wizardTowersAmount}`;
 
     if(cookies >= `${autoBakersPrice}`) {
         document.getElementById("auto-bakers-heading").style.color = "lime";
@@ -202,6 +367,23 @@ window.setInterval(function() {
         document.getElementById("cookie-factories-heading").style.color = "red";
     }
 
+    if(cookies >= `${cookieBanksPrice}`) {
+        document.getElementById("cookie-banks-heading").style.color = "lime";
+    } else {
+        document.getElementById("cookie-banks-heading").style.color = "red";
+    }
+
+    if(cookies >= `${cookieTemplesPrice}`) {
+        document.getElementById("cookie-temples-heading").style.color = "lime";
+    } else {
+        document.getElementById("cookie-temples-heading").style.color = "red";
+    }
+
+    if(cookies >= `${wizardTowersPrice}`) {
+        document.getElementById("wizard-towers-heading").style.color = "lime";
+    } else {
+        document.getElementById("wizard-towers-heading").style.color = "red";
+    }
 }, 250);
 
 // Using lime instead of green in the conditionals because it looks a lot nicer and less muddy
